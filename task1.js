11fda24236c6bde7d7af391c3cdf3e1b55563b60
tanveerhasanrishad <tanveerrishad007@gmail.com>
2026-02-05 18:01:53 +0600

    completed task 1

---
+++ task1.js
@@ -0,0 +1,10 @@
+const colors = ["red", "blue", "green", "yellow", "orange"];
+
+const rev_colors = [];
+
+for (let ele of colors) {
+  rev_colors.unshift(ele);
+}
+
+console.log(colors);
+console.log(rev_colors);
