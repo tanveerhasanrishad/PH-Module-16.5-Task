8a9b30b88a55a6e3e9055961ab48ffd00ec3a6c2
tanveerhasanrishad <tanveerrishad007@gmail.com>
2026-02-05 21:06:14 +0600

    completed task 6

---
+++ task6.js
@@ -0,0 +1,14 @@
+const students = [
+  {
+    name: "John",
+    marks: 85,
+  },
+  {
+    name: "Alice",
+    marks: 90,
+  },
+];
+
+for (let student of students) {
+  console.log(student.name + " scored " + student.marks);
+}
